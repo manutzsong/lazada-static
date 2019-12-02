97aacababf6bbd1d26459c339e85a0d42c53c762
manutzsong <manutzsong@gmail.com>
2019-12-02 19:04:49 +0700

    Updates

---
+++ precache-manifest.9d5701bab5942b2570e529e9faf7af14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efee7d73c1f11e4e3a7c561abd601128",
+    "url": "/lazada-static/index.html"
+  },
+  {
+    "revision": "ece463f7a281cba1641d",
+    "url": "/lazada-static/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "55545f5999f1b7b12f61",
+    "url": "/lazada-static/static/css/main.e97e6dc7.chunk.css"
+  },
+  {
+    "revision": "ece463f7a281cba1641d",
+    "url": "/lazada-static/static/js/2.eddb233f.chunk.js"
+  },
+  {
+    "revision": "55545f5999f1b7b12f61",
+    "url": "/lazada-static/static/js/main.6f11a486.chunk.js"
+  },
+  {
+    "revision": "dbb714cd2ca1eb88b44e",
+    "url": "/lazada-static/static/js/runtime-main.92e213f7.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/lazada-static/precache-manifest.9d5701bab5942b2570e529e9faf7af14.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lazada-static/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});

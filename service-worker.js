923a359e4bc456e7e3b7bdfe43313f7d91ad7964
manutzsong <manutzsong@gmail.com>
2019-12-02 20:05:22 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lazada-static/precache-manifest.9d5701bab5942b2570e529e9faf7af14.js"
+  "/lazada-static/precache-manifest.c19efe4e3ced4b8978559b709e8cde20.js"
 );
 
 self.addEventListener('message', (event) => {
